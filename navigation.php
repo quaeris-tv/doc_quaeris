56e2e6e78d115d935a0def01f86708059dadc31a
Malebestia <vair81@gmail.com>
2023-10-19 17:19:27 +0200

    .

--- navigation.php
+++ navigation.php
@@ -2,12 +2,12 @@
 
 return [
     'Getting Started' => [
-        'url' => 'doc_quaeris/docs/getting-started',
+        'url' => 'docs/getting-started',
         'children' => [
-            'Customizing Your Site' => 'doc_quaeris/docs/customizing-your-site',
-            'Navigation' => 'doc_quaeris/docs/navigation',
-            'Algolia DocSearch' => 'doc_quaeris/docs/algolia-docsearch',
-            'Custom 404 Page' => 'doc_quaeris/docs/custom-404-page',
+            'Customizing Your Site' => 'docs/customizing-your-site',
+            'Navigation' => 'docs/navigation',
+            'Algolia DocSearch' => 'docs/algolia-docsearch',
+            'Custom 404 Page' => 'docs/custom-404-page',
         ],
     ],
     'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
