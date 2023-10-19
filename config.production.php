d28d06fbb86d8b75db16bd07dd2477ded44307e3
marco76tv <marco.sottana@gmail.com>
2023-10-19 20:41:38 +0200

    first

---
+++ config.production.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'baseUrl' => 'https://quaeris-tv.github.io/doc_quaeris',
+    'production' => true,
+
+    // DocSearch credentials
+    'docsearchApiKey' => env('DOCSEARCH_KEY'),
+    'docsearchIndexName' => env('DOCSEARCH_INDEX'),
+];
